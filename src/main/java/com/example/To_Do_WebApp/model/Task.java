2f8ca2bde37ba6c67954334082b428a88f5ab716
Ali-Hafeez <ali.hafeez-buttar@city.ac.uk>
2024-10-03 21:31:16 +0100

    task initiation

---
+++ src/main/java/com/example/To_Do_WebApp/model/Task.java
@@ -0,0 +1,12 @@
+package com.example.To_Do_WebApp.model;
+
+
+public class Task {
+
+    private Long id;
+
+    private String description;
+    private boolean completed;
+
+    // Getters and Setters
+}
